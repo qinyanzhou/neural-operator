27451a44da3fad732745b8d39da969a73699c768
qinyanzhou <126661554+qinyanzhou@users.noreply.github.com>
2023-03-16 22:14:32 +0100

    Delete neuralop directory

--- neuralop/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tfno import TFNO, TFNO1d, TFNO2d, TFNO3d
-from .tfno import FNO, FNO1d, FNO2d, FNO3d
-
-from .model_dispatcher import get_model
--- neuralop/training/__init__.py
+++
@@ -1,3 +0,0 @@
-from .losses import LpLoss, H1Loss, H2Loss
-from .trainer import Trainer
-from .torch_setup import setup
